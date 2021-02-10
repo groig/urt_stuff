27a04a75d18da04f9a11ca0ccabbf9d4c995e445
Roig <groig@protonmail.com>
2021-02-10 09:49:46 -0500

    get stuff out of the loop

--- web_stats/gen.py
+++ web_stats/gen.py
@@ -2,15 +2,16 @@ import sqlite3
 import jinja2
 from time import sleep
 
+template_loader = jinja2.FileSystemLoader(searchpath="./")
+template_env = jinja2.Environment(loader=template_loader)
+template = template_env.get_template("index.j2")
+conn = sqlite3.connect("data.sqlite")
+c = conn.cursor()
+
 def main():
     print("generating")
-    conn = sqlite3.connect("data.sqlite")
-    c = conn.cursor()
     c.execute("SELECT name, num_played, kills, deaths, headshots, max_kill_streak, suicides, ratio, rounds FROM xlrstats ORDER BY ratio DESC")
     data = c.fetchall()
-    template_loader = jinja2.FileSystemLoader(searchpath="./")
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template("index.j2")
     output_text = template.render(data=data)
     with open("index.html", "w") as fh:
         fh.write(output_text)
