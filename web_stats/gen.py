47850cbf52e32e994fdaf6fd2f90149b3b8d8387
Roig <groig@protonmail.com>
2021-02-11 17:35:49 -0500

    moar data

--- web_stats/gen.py
+++ web_stats/gen.py
@@ -10,11 +10,33 @@ c = conn.cursor()
 
 def main():
     print("generating")
-    c.execute("SELECT name, num_played, kills, deaths, headshots, max_kill_streak, suicides, ratio, rounds FROM xlrstats ORDER BY ratio DESC")
-    data = c.fetchall()
-    output_text = template.render(data=data)
+
+    general_data = c.execute("SELECT name, num_played, kills, deaths, headshots, max_kill_streak, suicides, ratio, rounds FROM xlrstats ORDER BY ratio DESC").fetchall()
+
+    favorite_weapons = c.execute("select fragger, weapon, count(*) as frags from frags group by lower(fragger), lower(weapon) order by lower(fragger) asc, count(*) desc").fetchall()
+
+    frags_repartition = c.execute("select fragger, fragged, count(*) as frags from frags group by lower(fragger), lower(fragged) order by lower(fragger) asc, count(*) desc").fetchall()
+    frags_data = {}
+    for frag in frags_repartition:
+        if not frag[0] in frags_data:
+            frags_data[frag[0]] = [(frag[1], frag[2])]
+        else:
+            frags_data[frag[0]].append((frag[1], frag[2]))
+
+    deaths_repartition = c.execute("select fragged, fragger, count(*) from frags group by lower(fragged), lower(fragger) order by lower(fragged) asc, count(*) desc").fetchall()
+
+    deaths_data = {}
+    for frag in deaths_repartition:
+        if not frag[0] in deaths_data:
+            deaths_data[frag[0]] = [(frag[1], frag[2])]
+        else:
+            deaths_data[frag[0]].append((frag[1], frag[2]))
+
+    output_text = template.render(general_data=general_data, favorite_weapons=favorite_weapons, frags_repartition=frags_data, deaths_repartition=deaths_data)
+
     with open("index.html", "w") as fh:
         fh.write(output_text)
 
 if __name__ == "__main__":
         main()
+
